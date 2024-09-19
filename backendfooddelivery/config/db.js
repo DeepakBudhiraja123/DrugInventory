292bffbd916e784f92284ab40213252e5806ac61
Deepak Budhiraja <148183608+DeepakBudhiraja123@users.noreply.github.com>
2024-09-19 10:21:13 +0530

    Update db.js

--- backendfooddelivery/config/db.js
+++ backendfooddelivery/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://budhirajadeepak123:TTvqkb6w2hwbufsE@cluster0.covto.mongodb.net/?"
+    "mongodb+srv://budhirajadeepak123:<password>@cluster0.covto.mongodb.net/?"
   ).then(()=>{
     console.log("DB connected");
   });
